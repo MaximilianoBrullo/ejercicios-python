82f843bd861e1c52ff4bab00bb0fdce5f7cf374b
Maximliano Brullo <maximilianobrullo4@gmail.com>
2025-05-30 22:24:41 -0300

    tipo dato

--- lecturaEscrituraConsola.py
+++ lecturaEscrituraConsola.py
@@ -1,3 +1,7 @@
 nombre = input('ingrea nombre')
 edad = input('ingresa edad')
 print('el nombre es: ' + nombre +', tiene '+edad+ 'anios de edad')
+
+# aca se muestra el tipo de dato
+numero = 5
+print(type(nombre))
